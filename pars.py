344ebf74b8247631b1a2e8975a60c0451f82918c
alex <sklif.85@gmail.com>
2017-08-02 12:21:33 +0300

    db

--- pars.py
+++ pars.py
@@ -1,4 +1,4 @@
-# bin/user/python
+#!/bin/user/python
 
 from __future__ import print_function
 import fileinput
@@ -6,23 +6,39 @@ import readline
 import linecache
 import re
 import datetime
+from peewee import *
 
-####GlobalVar################################################
-MONTH = {'01':'January', '02':'February', '03':'March', '4':'April', '5':'May', '6':'June', '07':'July', '8':'August', '9':'September', '10':'October', '11':'November', '12':'December'}
+#### GlobalVar ########################################################################################################
+MONTH = {'01':'January', '02':'February', '03':'March', '4':'April', '5':'May', '6':'June',
+         '07':'July', '8':'August', '9':'September', '10':'October', '11':'November', '12':'December'}
 WEEKDAY = {0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday',4:'Fraiday', 5:'Saturday', 6:'Sunday'}
 ernings_type = None
 date = None
-_len = 0
 id = 0
 erns = 0
 num = 0
-#############################################################
+#######################################################################################################################
 
 fp = "/Users/alex/PycharmProjects/untitled1/ernings_july_2017.txt"
 fd = open(fp, mode='r')
 len_lines = len(fd.readlines())
 fd.close()
 i = 0
+
+##### Connect to DB ##################################
+# mySQL_db = MySQLDatabase('db_mystock')
+#
+# class ernings(Model):
+#     month = CharField
+#     date = CharField
+#     weekday = CharField
+#     id = IntegerField
+#     ernings = FloatField
+#     downloads = IntegerField
+#     ernings_type = CharField
+#
+# mySQL_db.connect()
+
 with open(fp, mode='r') as f:
     line = f.readlines()
     for i in range(0, len_lines):
@@ -30,13 +46,17 @@ with open(fp, mode='r') as f:
             date = line[i+1].split('=')[2].split('&')[0].split('-')
             date = date[2] + '/' + date[1] + '/' + date[0]
             ernings_type = line[i+2].strip().split(' ')[0]
-            _len = int(line[i+2].strip().split(' ')[1].strip('(').strip(')'))
         if re.findall(r'\$+[0-9]+\.+[0-9]+[0-9]', line[i]):
             id = line[i-1].split('<')[1].split('>')[1]
             erns = float(line[i].split('<')[1].split('$')[1])
             num = int(line[i+1].split('<')[1].split('>')[1])
             month = MONTH.get(date.split('/')[1])
             weekday = WEEKDAY.get(datetime.datetime(int(date.split('/')[2]), int(date.split('/')[1].split('0')[1]), int(date.split('/')[0])).weekday())
+
+            # _ernings = ernings(month=month, date=date, weekday=weekday, id=id, ernings=erns, downloads=num, ernings_type=ernings_type)
+            # _ernings.save()
+
+
             print(month, end=' ')
             print(date, end=' ')
             print(weekday, end=' ')
@@ -44,3 +64,6 @@ with open(fp, mode='r') as f:
             print(erns, end=' ')
             print(num, end=' ')
             print(ernings_type, end='\n')
+
+
+
--- test2.py
+++
@@ -1,9 +0,0 @@
-# bin/user/python
-
-import urllib3
-
-url ='http://urllib3.readthedocs.org/'
-http_pool = urllib3.connection_from_url(url)
-r = http_pool.urlopen('GET', url)
-
-print (r.data)
