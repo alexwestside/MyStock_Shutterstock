0a3267382e3d1238e06ee375fb864ec425026691
Olexandr Rizhiy <orizhiy@student.unit.ua>
2017-08-20 22:32:49 +0300

    ___

---
+++ 21323.py
@@ -0,0 +1,13 @@
+# ast = {"access_token":"1/eyJjbGllbnRfaWQiOiI2NjExZi1mOTg2Mi01YjRkMS1iNzc4OS02YjJhYS1lZTc0NSIsInJlYWxtIjoiY3VzdG9tZXIiLCJzY29wZSI6InVzZXIudmlldyIsInV0diI6IjJlYWQwYzhkNDMiLCJ1c2VybmFtZSI6InlhbnVzaGtvdiIsInVzZXJfaWQiOjExMjQzOTg0NSwib3JnYW5pemF0aW9uX2lkIjpudWxsLCJjdXN0b21lcl9pZCI6NDQ3NjQzMCwiZXhwIjoxNTAzMjU0NTMwfQ.RSiVJeS-vFOl3bQ0rfNyGpap0QSHBi6Tc2jhpV-Oo6KfmwM4yU_hRPszhZ94wNUGcuMjnz-K3RXAeN6A0K0jbA","token_type":"Bearer"}
+#
+# at='1/eyJjbGllbnRfaWQiOiI2NjExZi1mOTg2Mi01YjRkMS1iNzc4OS02YjJhYS1lZTc0NSIsInJlYWxtIjoiY3VzdG9tZXIiLCJzY29wZSI6InVzZXIudmlldyIsInV0diI6IjJlYWQwYzhkNDMiLCJ1c2VybmFtZSI6InlhbnVzaGtvdiIsInVzZXJfaWQiOjExMjQzOTg0NSwib3JnYW5pemF0aW9uX2lkIjpudWxsLCJjdXN0b21lcl9pZCI6NDQ3NjQzMCwiZXhwIjoxNTAzMjU0NTMwfQ.RSiVJeS-vFOl3bQ0rfNyGpap0QSHBi6Tc2jhpV-Oo6KfmwM4yU_hRPszhZ94wNUGcuMjnz-K3RXAeN6A0K0jbA'
+# at2='2/vSrH7JUnbhRIrIPPi0dfm20paLFAQhTRlRYvVkRLkptldy6SvvCRTD1h8mQFrtgwOyQ0-REjVew2wZtiCOH9lOhmW6xE8T6UUIUi2ofAkFy3iMEPEbmbgziA_b08dWHkWQWn4C3x_yPE2_D95UsNIRTVpZtaA_874GygtwmourHe77fYpSgTmGV28jPg6Q0U'
+# __api = url+'?'+scope+'&'+res_t+'&'+ru+'&client_id='+cliendID+'&state=lol'
+# _api = url2+at+'/earnings'
+# api='https://api.shutterstock.com/v2/user?access_token='
+
+q = 'https://'+cliendID+':'+clientSecret+'@api.shutterstock.com/v2/user/subscriptions&scope=purchases.view'
+
+
+
+a= 'https://6611f-f9862-5b4d1-b7789-6b2aa-ee745:1f538-1ca8e-8bcb0-7d676-cdff7-08b6b@api.shutterstock.com/v2/user/subscriptions'
--- cookie/cookie.py
+++ cookie/cookie.py
@@ -124,12 +124,15 @@ scope='scope=licenses.create licenses.view purchases.view'
 res_t='response_type=code'
 ru='redirect_uri=localhost'
 
-ast = {"access_token":"1/eyJjbGllbnRfaWQiOiI2NjExZi1mOTg2Mi01YjRkMS1iNzc4OS02YjJhYS1lZTc0NSIsInJlYWxtIjoiY3VzdG9tZXIiLCJzY29wZSI6InVzZXIudmlldyIsInV0diI6IjJlYWQwYzhkNDMiLCJ1c2VybmFtZSI6InlhbnVzaGtvdiIsInVzZXJfaWQiOjExMjQzOTg0NSwib3JnYW5pemF0aW9uX2lkIjpudWxsLCJjdXN0b21lcl9pZCI6NDQ3NjQzMCwiZXhwIjoxNTAzMjU0NTMwfQ.RSiVJeS-vFOl3bQ0rfNyGpap0QSHBi6Tc2jhpV-Oo6KfmwM4yU_hRPszhZ94wNUGcuMjnz-K3RXAeN6A0K0jbA","token_type":"Bearer"}
+# ast = {"access_token":"1/eyJjbGllbnRfaWQiOiI2NjExZi1mOTg2Mi01YjRkMS1iNzc4OS02YjJhYS1lZTc0NSIsInJlYWxtIjoiY3VzdG9tZXIiLCJzY29wZSI6InVzZXIudmlldyIsInV0diI6IjJlYWQwYzhkNDMiLCJ1c2VybmFtZSI6InlhbnVzaGtvdiIsInVzZXJfaWQiOjExMjQzOTg0NSwib3JnYW5pemF0aW9uX2lkIjpudWxsLCJjdXN0b21lcl9pZCI6NDQ3NjQzMCwiZXhwIjoxNTAzMjU0NTMwfQ.RSiVJeS-vFOl3bQ0rfNyGpap0QSHBi6Tc2jhpV-Oo6KfmwM4yU_hRPszhZ94wNUGcuMjnz-K3RXAeN6A0K0jbA","token_type":"Bearer"}
+#
+# at='1/eyJjbGllbnRfaWQiOiI2NjExZi1mOTg2Mi01YjRkMS1iNzc4OS02YjJhYS1lZTc0NSIsInJlYWxtIjoiY3VzdG9tZXIiLCJzY29wZSI6InVzZXIudmlldyIsInV0diI6IjJlYWQwYzhkNDMiLCJ1c2VybmFtZSI6InlhbnVzaGtvdiIsInVzZXJfaWQiOjExMjQzOTg0NSwib3JnYW5pemF0aW9uX2lkIjpudWxsLCJjdXN0b21lcl9pZCI6NDQ3NjQzMCwiZXhwIjoxNTAzMjU0NTMwfQ.RSiVJeS-vFOl3bQ0rfNyGpap0QSHBi6Tc2jhpV-Oo6KfmwM4yU_hRPszhZ94wNUGcuMjnz-K3RXAeN6A0K0jbA'
+# at2='2/vSrH7JUnbhRIrIPPi0dfm20paLFAQhTRlRYvVkRLkptldy6SvvCRTD1h8mQFrtgwOyQ0-REjVew2wZtiCOH9lOhmW6xE8T6UUIUi2ofAkFy3iMEPEbmbgziA_b08dWHkWQWn4C3x_yPE2_D95UsNIRTVpZtaA_874GygtwmourHe77fYpSgTmGV28jPg6Q0U'
+# __api = url+'?'+scope+'&'+res_t+'&'+ru+'&client_id='+cliendID+'&state=lol'
+# _api = url2+at+'/earnings'
+# api='https://api.shutterstock.com/v2/user?access_token='
 
-at='1/eyJjbGllbnRfaWQiOiI2NjExZi1mOTg2Mi01YjRkMS1iNzc4OS02YjJhYS1lZTc0NSIsInJlYWxtIjoiY3VzdG9tZXIiLCJzY29wZSI6InVzZXIudmlldyIsInV0diI6IjJlYWQwYzhkNDMiLCJ1c2VybmFtZSI6InlhbnVzaGtvdiIsInVzZXJfaWQiOjExMjQzOTg0NSwib3JnYW5pemF0aW9uX2lkIjpudWxsLCJjdXN0b21lcl9pZCI6NDQ3NjQzMCwiZXhwIjoxNTAzMjU0NTMwfQ.RSiVJeS-vFOl3bQ0rfNyGpap0QSHBi6Tc2jhpV-Oo6KfmwM4yU_hRPszhZ94wNUGcuMjnz-K3RXAeN6A0K0jbA'
-at2='2/vSrH7JUnbhRIrIPPi0dfm20paLFAQhTRlRYvVkRLkptldy6SvvCRTD1h8mQFrtgwOyQ0-REjVew2wZtiCOH9lOhmW6xE8T6UUIUi2ofAkFy3iMEPEbmbgziA_b08dWHkWQWn4C3x_yPE2_D95UsNIRTVpZtaA_874GygtwmourHe77fYpSgTmGV28jPg6Q0U'
-__api = url+'?'+scope+'&'+res_t+'&'+ru+'&client_id='+cliendID+'&state=lol'
-_api = url2+at+'/earnings'
-api='https://api.shutterstock.com/v2/user?access_token='
-request = requests.get(api+ast)
+q = 'https://'+cliendID+':'+clientSecret+'@api.shutterstock.com/v2/user/subscriptions&scope=purchases.view'
+
+request = requests.post(q, json=)
 print (1)
