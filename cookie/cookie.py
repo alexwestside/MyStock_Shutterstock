31ebf53276670acdd041bf83d052a13327abd93f
Olexandr Rizhiy <orizhiy@student.unit.ua>
2017-08-20 21:07:12 +0300

    _

--- cookie/cookie.py
+++ cookie/cookie.py
@@ -97,7 +97,39 @@
 # print(1)
 
 import requests
-api = 'https://6611f-f9862-5b4d1-b7789-6b2aa-ee745:1f538-1ca8e-8bcb0-7d676-cdff7-08b6b@api.shutterstock.com/v2/images/search?query=donkey'
+# api = 'https://6611f-f9862-5b4d1-b7789-6b2aa-ee745:1f538-1ca8e-8bcb0-7d676-cdff7-08b6b@api.shutterstock.com/v2/images/search?query=donkey'
 
-request = requests.get(api)
+
+
+#
+# pre = 'https://'
+# api = '@api.shutterstock.com/v2/images/search?'
+# cliendID = '6611f-f9862-5b4d1-b7789-6b2aa-ee745'
+# reg = ':'
+# clientSecret = '1f538-1ca8e-8bcb0-7d676-cdff7-08b6b'
+# type1 = 'added_date_start='
+# page = 'page='
+#
+# _api = pre+cliendID+reg+clientSecret+api+type1+'2017-04-21&'+page+'80&per_page=500'
+#
+# q = 'https://'+cliendID+':'+clientSecret+'@api.shutterstock.com/v2/images/search?query=donkey'
+#
+# __api = pre+cliendID+reg+clientSecret+'@api.shutterstock.com/v2/user/subscriptions&scope=purchases.view'
+cliendID = '6611f-f9862-5b4d1-b7789-6b2aa-ee745'
+clientSecret = '1f538-1ca8e-8bcb0-7d676-cdff7-08b6b'
+
+url='https://api.shutterstock.com/v2/oauth/authorize'
+url2='https://api.shutterstock.com/'
+scope='scope=licenses.create licenses.view purchases.view'
+res_t='response_type=code'
+ru='redirect_uri=localhost'
+
+ast = {"access_token":"1/eyJjbGllbnRfaWQiOiI2NjExZi1mOTg2Mi01YjRkMS1iNzc4OS02YjJhYS1lZTc0NSIsInJlYWxtIjoiY3VzdG9tZXIiLCJzY29wZSI6InVzZXIudmlldyIsInV0diI6IjJlYWQwYzhkNDMiLCJ1c2VybmFtZSI6InlhbnVzaGtvdiIsInVzZXJfaWQiOjExMjQzOTg0NSwib3JnYW5pemF0aW9uX2lkIjpudWxsLCJjdXN0b21lcl9pZCI6NDQ3NjQzMCwiZXhwIjoxNTAzMjU0NTMwfQ.RSiVJeS-vFOl3bQ0rfNyGpap0QSHBi6Tc2jhpV-Oo6KfmwM4yU_hRPszhZ94wNUGcuMjnz-K3RXAeN6A0K0jbA","token_type":"Bearer"}
+
+at='1/eyJjbGllbnRfaWQiOiI2NjExZi1mOTg2Mi01YjRkMS1iNzc4OS02YjJhYS1lZTc0NSIsInJlYWxtIjoiY3VzdG9tZXIiLCJzY29wZSI6InVzZXIudmlldyIsInV0diI6IjJlYWQwYzhkNDMiLCJ1c2VybmFtZSI6InlhbnVzaGtvdiIsInVzZXJfaWQiOjExMjQzOTg0NSwib3JnYW5pemF0aW9uX2lkIjpudWxsLCJjdXN0b21lcl9pZCI6NDQ3NjQzMCwiZXhwIjoxNTAzMjU0NTMwfQ.RSiVJeS-vFOl3bQ0rfNyGpap0QSHBi6Tc2jhpV-Oo6KfmwM4yU_hRPszhZ94wNUGcuMjnz-K3RXAeN6A0K0jbA'
+at2='2/vSrH7JUnbhRIrIPPi0dfm20paLFAQhTRlRYvVkRLkptldy6SvvCRTD1h8mQFrtgwOyQ0-REjVew2wZtiCOH9lOhmW6xE8T6UUIUi2ofAkFy3iMEPEbmbgziA_b08dWHkWQWn4C3x_yPE2_D95UsNIRTVpZtaA_874GygtwmourHe77fYpSgTmGV28jPg6Q0U'
+__api = url+'?'+scope+'&'+res_t+'&'+ru+'&client_id='+cliendID+'&state=lol'
+_api = url2+at+'/earnings'
+api='https://api.shutterstock.com/v2/user?access_token='
+request = requests.get(api+ast)
 print (1)
