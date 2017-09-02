2f24f267ccca635ee35cb0383ce4054750324d6b
Olexandr Rizhiy <orizhiy@student.unit.ua>
2017-09-02 20:56:34 +0300

    parse_portfolio_table

---
+++ pars_portfolio_table.py
@@ -0,0 +1,10 @@
+
+
+import re
+
+with open('pars_portfolio_table.txt', 'r') as f:
+    line = f.readlines()
+    for l in line:
+        l = l.strip().split('. ')[1]
+        l = re.split('(\d+)', l)
+        print (l[0])
