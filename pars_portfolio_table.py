91f658886d361948ff9eed6ee88682d1962fc6fa
alex <sklif.85@gmail.com>
2017-09-26 21:25:09 +0300

    init struct

--- pars_portfolio_table.py
+++
@@ -1,9 +0,0 @@
-
-import re
-
-with open('pars_portfolio_table.txt', 'r') as f:
-    line = f.readlines()
-    for l in line:
-        l = l.strip().split('. ')[1]
-        l = re.split('(\d+)', l)
-        print (l[1])
