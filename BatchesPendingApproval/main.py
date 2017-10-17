cd52c5956690cd5f4ac976f4a02029c8a3dad267
Vadim Rozov <vadimrozov@gmail.com>
2017-10-17 22:46:43 +0300

    Added rejected statistic

--- BatchesPendingApproval/main.py
+++ BatchesPendingApproval/main.py
@@ -5,7 +5,8 @@ from peewee import *
 from bs4 import BeautifulSoup
 
 
-INIT_APPROVED_PHOTOS_URL = "https://submit.shutterstock.com/review.mhtml?type=photos"
+APPROVED_PHOTOS_URL = "https://submit.shutterstock.com/review.mhtml?type=photos"
+REJECTED_PHOTOS_URL = "https://submit.shutterstock.com/review.mhtml?approved=-1&type=photos"
 batches_db = SqliteDatabase('batches_statistic.db')
 
 
@@ -13,9 +14,10 @@ class BatchInfo(Model):
     id = IntegerField(primary_key=True)
     submitted = DateField()
     items = IntegerField()
+    rejected_items = IntegerField()
 
     def __repr__(self):
-        return "{id: " + str(self.id) + " submitted: " + str(self.submitted) + " items: " + str(self.items) + "}"
+        return "{id: " + str(self.id) + " submitted: " + str(self.submitted) + " items: " + str(self.items) + " rejected: " + str(self.rejected_items) + "}"
 
     class Meta:
         database = batches_db
@@ -56,6 +58,53 @@ class BatchesPendingApprovalWrapper:
         return batches
 
 
+class BatchesRejectedWrapper:
+    def __init__(self, login_credentials, url):
+        self.url = url
+        self.login_credentials = login_credentials
+
+    def get_response(self):
+        return requests.get(self.url, cookies=self.login_credentials.cookies)
+
+    def get_batches(self):
+        html = self.get_response().text
+        soup_talbe = BeautifulSoup(html, 'html.parser')
+        tr_search_results = soup_talbe.find('table', class_="table").find_all('tr');
+
+        batches = []
+
+        for tr_search_result in tr_search_results:
+            td_search_results = tr_search_result.find_all('td')
+
+            # delete last line in table, because it is paginator
+            if len(td_search_results) < 3:
+                continue
+
+            id = int(td_search_results[0].get_text())
+            submitted = datetime.datetime.strptime(td_search_results[1].get_text(), '%m/%d/%Y').date()
+            rejected_items = int(td_search_results[2].get_text())
+            batches.append(BatchInfo(id=id, submitted=submitted, rejected_items=rejected_items))
+
+        return batches
+
+
+def save_rejected_to_db(batches):
+    batches_db.connect()
+    if not BatchInfo.table_exists():
+        batches_db.close()
+        return
+
+    for batch_sh in batches:
+        batch_db = BatchInfo.select().where(BatchInfo.id == batch_sh.id)
+        if batch_db.exists():
+            res = batch_db.get()
+            res.rejected_items = batch_sh.rejected_items
+            saved = res.save()
+            print saved, batch_sh.id, "updated with rejections"
+
+    batches_db.close()
+
+
 def save_to_db(batches):
     batches_db.connect()
     if not BatchInfo.table_exists():
@@ -64,8 +113,8 @@ def save_to_db(batches):
     for batch_sh in batches:
         query = BatchInfo.select().where(BatchInfo.id == batch_sh.id)
         if not query.exists():
-            saved = BatchInfo.create(id=batch_sh.id, submitted=batch_sh.submitted, items=batch_sh.items)
-            print saved
+            saved = BatchInfo.create(id=batch_sh.id, submitted=batch_sh.submitted, items=batch_sh.items, rejected_items=0)
+            print saved, "added to db"
 
     batches_db.close()
 
@@ -79,17 +128,23 @@ def get_batches_by_date(date_submitted):
 
 def main():
     yanushkov_login = LoginCredentials(browsercookie.chrome())
-    yanushkov_request_wrapper = BatchesPendingApprovalWrapper(yanushkov_login, INIT_APPROVED_PHOTOS_URL)
-    batches = yanushkov_request_wrapper.get_batches()
+
+    yanushkov_pending_wrapper = BatchesPendingApprovalWrapper(yanushkov_login, APPROVED_PHOTOS_URL)
+    batches = yanushkov_pending_wrapper.get_batches()
     save_to_db(batches)
 
+    yanushkov_rejected_wrapper = BatchesRejectedWrapper(yanushkov_login, REJECTED_PHOTOS_URL)
+    batches = yanushkov_rejected_wrapper.get_batches()
+    save_rejected_to_db(batches)
+
     total = 0
+    rejected = 0
     filter_date = datetime.datetime.strptime("05/02/2017", '%m/%d/%Y').date()
     batches = get_batches_by_date(filter_date)
     for batch_sh in batches:
         total += batch_sh.items
-        print batch_sh.id, batch_sh.submitted, batch_sh.items
+        rejected += batch_sh.rejected_items
 
-    print "Date:", filter_date, "submitted files:", total
+    print "Date:", filter_date, "submitted files:", total, "rejected files:", rejected
 
 main()
