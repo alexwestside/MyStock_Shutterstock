71d856d45f752893e2e4dea2bd4f19c19ab794bd
Vadim Rozov <vadimrozov@gmail.com>
2017-10-17 19:16:47 +0300

    Parsed and stored in db

--- BatchesPendingApproval/main.py
+++ BatchesPendingApproval/main.py
@@ -1,10 +1,24 @@
 import requests
 import browsercookie
 import datetime
+from peewee import *
 from bs4 import BeautifulSoup
 
 
 INIT_APPROVED_PHOTOS_URL = "https://submit.shutterstock.com/review.mhtml?type=photos"
+batches_db = SqliteDatabase('batches_statistic.db')
+
+
+class BatchInfo(Model):
+    id = IntegerField(primary_key=True)
+    submitted = DateField()
+    items = IntegerField()
+
+    def __repr__(self):
+        return "{id: " + str(self.id) + " submitted: " + str(self.submitted) + " items: " + str(self.items) + "}"
+
+    class Meta:
+        database = batches_db
 
 
 class LoginCredentials:
@@ -24,22 +38,58 @@ class BatchesPendingApprovalWrapper:
         html = self.get_response().text
         soup_talbe = BeautifulSoup(html, 'html.parser')
         tr_search_results = soup_talbe.find('table', class_="table").find_all('tr');
+
+        batches = []
+
         for tr_search_result in tr_search_results:
             td_search_results = tr_search_result.find_all('td')
 
-            # удаляем последнюю строчку в таблице, так как это пейджинатор
+            # delete last line in table, because it is paginator
             if len(td_search_results) < 3:
                 continue
 
-            print(int(td_search_results[0].get_text()))
-            print(datetime.datetime.strptime(td_search_results[1].get_text(), '%m/%d/%Y').date())
-            print(int(td_search_results[2].get_text()))
+            id = int(td_search_results[0].get_text())
+            submitted = datetime.datetime.strptime(td_search_results[1].get_text(), '%m/%d/%Y').date()
+            items = int(td_search_results[2].get_text())
+            batches.append(BatchInfo(id=id, submitted=submitted, items=items))
+
+        return batches
+
+
+def save_to_db(batches):
+    batches_db.connect()
+    if not BatchInfo.table_exists():
+        batches_db.create_table(BatchInfo)
+
+    for batch_sh in batches:
+        query = BatchInfo.select().where(BatchInfo.id == batch_sh.id)
+        if not query.exists():
+            saved = BatchInfo.create(id=batch_sh.id, submitted=batch_sh.submitted, items=batch_sh.items)
+            print saved
+
+    batches_db.close()
+
+
+def get_batches_by_date(date_submitted):
+    batches_db.connect()
+    query = BatchInfo.select().where(BatchInfo.submitted == date_submitted)
+    batches_db.close()
+    return query
 
 
 def main():
     yanushkov_login = LoginCredentials(browsercookie.chrome())
     yanushkov_request_wrapper = BatchesPendingApprovalWrapper(yanushkov_login, INIT_APPROVED_PHOTOS_URL)
-    yanushkov_request_wrapper.get_batches()
-    # print yanushkov_request_wrapper.get_response().text
+    batches = yanushkov_request_wrapper.get_batches()
+    save_to_db(batches)
+
+    total = 0
+    filter_date = datetime.datetime.strptime("05/02/2017", '%m/%d/%Y').date()
+    batches = get_batches_by_date(filter_date)
+    for batch_sh in batches:
+        total += batch_sh.items
+        print batch_sh.id, batch_sh.submitted, batch_sh.items
+
+    print "Date:", filter_date, "submitted files:", total
 
 main()
